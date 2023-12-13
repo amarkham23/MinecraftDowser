c9491c2c6cefd41592f02e8741020905b702d4cd
Andrew Markham <andrew@markham.ltd>
2023-12-13 14:54:18 -0500

    Add files via upload

---
+++ NetheriteDowser/src/main/java/com/andrewmarkham/netheritedowser/Dowser.java
@@ -0,0 +1,9 @@
+package com.andrewmarkham.netheritedowser;
+
+import net.minecraft.world.item.Item;
+
+public class Dowser extends Item {
+    public Dowser(Properties properties) {
+        super(properties);
+    }
+}
