9c14025d3013e0f748a3c4295d728e8d7ea04f9d
andrewM <andrew.markham03@gmail.com>
2023-12-13 15:00:23 -0500

    Updated file structure

--- NetheriteDowser/src/main/java/com/andrewmarkham/netheritedowser/Dowser.java
+++
@@ -1,9 +0,0 @@
-package com.andrewmarkham.netheritedowser;
-
-import net.minecraft.world.item.Item;
-
-public class Dowser extends Item {
-    public Dowser(Properties properties) {
-        super(properties);
-    }
-}
